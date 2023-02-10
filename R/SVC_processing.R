3c0bfd502c20bd2a27c1110b9c4cae7b821f87f4
WongCYS <84600659+WongCYS@users.noreply.github.com>
2023-02-10 13:55:34 -0800

    Ready to go

--- R/SVC_processing.R
+++ R/SVC_processing.R
@@ -1,3 +1,5 @@
+#Reads in SVC data
+
 library(tidyverse)
 
 #Location of all the raw .sig files 
@@ -9,7 +11,6 @@ dataset <- data.frame()
 for (i in 1:length(file_list)){
   temp_data <- read.csv2(file_list[i], sep = "", header=F, skip = 30)
   temp_data[1:4] <- lapply(temp_data[1:4], as.numeric)
-  #temp_data <- as.rspec(temp_data,whichwl = "V1") #For interpolating spectra to 1 nm resolution; needs the pavo library
   temp_data$id <- file_list2[i]
   print(paste(file_list2[i], which(file_list == file_list[i]), "out of", length(file_list)))
   dataset <- rbind(dataset, temp_data)
@@ -19,9 +20,10 @@ for (i in 1:length(file_list)){
 names(dataset) <- c("wavelength", "reference", "radiance", "reflectance", "scan")
 
 #Plot all spectra
-ggplot(dataset, aes(x=wavelength,y=reflectance, color = scan, group = scan)) +   #Or color = ID
+ggplot(dataset, aes(x = wavelength,y = reflectance, color = scan, group = scan)) +   #Or color = ID
   geom_line(show.legend = F,size=.5)+
   scale_x_continuous("Wavelength (nm)",limits = c(300,2500), breaks = seq(400,2500,200))+
+  scale_y_continuous("Reflectance (%)")
   theme_bw()+
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
 
@@ -29,11 +31,21 @@ ggplot(dataset, aes(x=wavelength,y=reflectance, color = scan, group = scan)) +  
 df_wide <- dataset %>% select(wavelength, reflectance, scan) %>%
   pivot_wider(names_from = wavelength, values_from = reflectance, id_cols = scan, values_fn = mean)
 
-#Calculate some vegetation indices
-df_wide$NDVI <- (df_wide$`800.9` - df_wide$`680.8`) / (df_wide$`800.9` + df_wide$`680.8`)
-df_wide$PRI <- (df_wide$`531.3` - df_wide$`570.4`) / (df_wide$`531.3` + df_wide$`570.4`)
+#Define function to calculate vegetation indices
+#calculates vegetation index using (a:b - c:d) / (a:b + c:d)
+calc_VI <- function(df, a, b, buffer1, buffer2){
+  b1 <- which.min(abs(a - as.numeric(names(df), options(warn=-1))))   #Identify first band and start range
+  b2 <- which.min(abs(b - as.numeric(names(df), options(warn=-1))))   #Identify second band and end range
+  ((rowMeans(df_wide[b1-buffer1:b1+buffer1]) - rowMeans(df_wide[b2-buffer2:b2+buffer2])) /      #vegetation index equation
+      (rowMeans(df_wide[b1-buffer1:b1+buffer1]) + rowMeans(df_wide[b2-buffer2:b2+buffer2])))
+}
 
-#plot PRI and NDVI plot to look at spread
+#Example NDVI red 620 to 670 nm and NIR 841 to 876 nm
+df_wide$NDVI <- calc_VI(df_wide, 870, 680, 20, 20)   
+#Example PRI r531 526 to 536 nm and r570 565 to 575 nm
+df_wide$PRI <- calc_VI(df_wide, 531, 570, 2, 2)
+
+#Quick PRI and NDVI plot to look at spread
 ggplot(df_wide,aes(x = scan,y = PRI))+      
   geom_point()+
   scale_y_continuous(bquote("PRI"))+
@@ -49,4 +61,4 @@ ggplot(df_wide,aes(x = scan,y = NDVI))+
 
 #export csv
 #write.csv(dataset,'20211004_long.csv', row.names = FALSE)        #Long format
-#write.csv(df_wide,'20211004_wide.csv', row.names = FALSE)         #Wide format
+write.csv(df_wide,'20211004_wide.csv', row.names = FALSE)         #Wide format
